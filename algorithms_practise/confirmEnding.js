e96d8f025523e83147865299e053880770ac84b6
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-10 23:12:45 +0300

    confirm ending of a string

---
+++ algorithms_practise/confirmEnding.js
@@ -0,0 +1,12 @@
+// Check if a string(first argument, str)ends with the given target
+// string(second argument, target)     .This challenge can be solved with the
+//  .endsWith()method, which was introduced in ES2015.But for the purpose of
+// this challenge, we would like you to use one of the JavaScript substring
+// methods instead.
+
+const confirmEnding = (string, endsWith) => string.endsWith(endsWith);
+const confirmEndingAlt = (string, endsWith) => {
+    return string.substring(string.length-endsWith.length)===endsWith;
+};
+
+console.log(confirmEnding('Oyelowo', 'owo'));
