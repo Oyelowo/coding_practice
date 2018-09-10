e56043c718434b238392706db99b9c341b90e00b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-10 23:03:26 +0300

    length of longest word

--- algorithms_practise/longestWord.js
+++ algorithms_practise/longestWord.js
@@ -1,2 +1,6 @@
 // Return the length of the longest word in the provided sentence.Your response
 // should be a number.
+
+const longestWord = (sentence) =>sentence.split(' ').sort((a,b)=> b.length-a.length)[0].length;
+
+console.log(longestWord('Return the length of the longest word in the provided sentence'));
