857fd6ba78dc40a61417b20b57fd1ab8fa189e2f
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-11 21:37:08 +0300

    mutation

--- algorithms_practise/findElement.js
+++ algorithms_practise/findElement.js
@@ -10,5 +10,5 @@ const findElement = (array, func) => {
     }; 
 };
 
-findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
 console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }));
