12e884b1836236e95af108a9ff12d3f117cdaec9
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-11 21:37:46 +0300

    mutation

--- algorithms_practise/mutation.js
+++ algorithms_practise/mutation.js
@@ -5,3 +5,30 @@
 //    "hey"]should return false because the string "hello" does not contain a
 // "y".Lastly, [     "Alien", "line" ], should return true because all of the
 // letters in "line" are present in "Alien".
+
+const mutation = (arr) => {
+    let [firstElement, secondElement]= arr;
+    firstElement = firstElement.toLowerCase().split('');
+    secondElement = secondElement.toLowerCase().split('');
+   return secondElement.every(second=> firstElement.some(first=> first===second));
+};
+
+// longerSolution
+// 
+// const mutation = (arr) => {
+//     let [firstElement, secondElement]= arr;
+//     firstElement = firstElement.toLowerCase().split('');
+//     secondElement = secondElement.toLowerCase().split('');
+//     let array=[];
+//   for(let item of secondElement) {
+//      array=array.concat(firstElement.some(first=>first===item));
+//   }
+//   if (array.some(el=> el===false)) {
+//       return false;
+//   };
+//   return true 
+// };
+
+ console.log(mutation(["hello", "hlle"]));
+ console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+ console.log(mutation(["Mary", "Aarmy"]));
