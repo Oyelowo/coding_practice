803be8af598c3a1507cd9a856f3de07ce8a0f2d7
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-11 22:49:50 +0300

    booWho check if boolean and also remove falsy

--- algorithms_practise/booWho.js
+++ algorithms_practise/booWho.js
@@ -1,6 +1,11 @@
 // Check if a value is classified as a boolean primitive. Return true or false.
 // Boolean primitives are true and false.
 
-const booWho = (bool) => {
-  
-}
+const booWho = (bool) =>bool===true || bool===false;
+ 
+console.log( booWho(true));
+console.log( booWho('true'));
+console.log( booWho(false));
+console.log( booWho('false'));
+console.log(booWho([].slice));
+console.log(booWho(NaN));
