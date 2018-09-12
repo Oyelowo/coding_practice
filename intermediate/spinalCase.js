55475d3795fb75bd000678a86658816022588505
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-12 21:53:32 +0300

    check if uppercase

--- intermediate/spinalCase.js
+++ intermediate/spinalCase.js
@@ -1,8 +1,19 @@
-// Spinal Tap Case
-// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+// Spinal Tap Case Convert a string to spinal case. Spinal case is
+// all-lowercase-words-joined-by-dashes.
 
+const isUpperCase = (letter) => letter === letter.toUpperCase() && !typeof(letter) === 'number';
 
-const spinalCase = (str) => str.split(/\W|[A-Z]/).join('-').toLowerCase();
-  
- console.log(spinalCase('This Is Spinal Tap'));
- console.log(spinalCase("AllThe-small Things"));
+// console.log(isUpperCase('g'));
+
+const spinalCase = (str) => {
+    for(let item of str) {
+      if(isUpperCase(item)){
+          
+      }
+    }
+    
+   return str.split(/\W/).join('-')
+};
+
+console.log(spinalCase('This Is Spinal Tap'));
+console.log(spinalCase("AllThe-small Things"));
