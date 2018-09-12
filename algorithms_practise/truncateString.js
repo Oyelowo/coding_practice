790c0f8a75c31fb0f7fdf2500bb18c61ed7753f7
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-12 09:12:25 +0300

    truncateString function corrected

--- algorithms_practise/truncateString.js
+++ algorithms_practise/truncateString.js
@@ -4,6 +4,7 @@
 
 const truncateString = (str, num) => str.length >num ?`${str.substring(0, num + 1)}...`: str;
 
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11))
 console.log( truncateString("A-tisket a-tasket A green and yellow basket", 8));
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2));
 console.log(truncateString("A-", 1));
