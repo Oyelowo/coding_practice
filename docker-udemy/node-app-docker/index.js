e24993660d9759e696edf690d7a13d51d9bab6c2
oyelowo <oyelowo.oyedayo@relex.fi>
2020-03-06 20:38:24 +0200

    copying files separately to prevent unnecessary cache oinvalidation especially of package.json file

--- docker-udemy/node-app-docker/index.js
+++ docker-udemy/node-app-docker/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there!");
+  res.send("The way you code!");
 });
 
 app.listen(8080, () => {
