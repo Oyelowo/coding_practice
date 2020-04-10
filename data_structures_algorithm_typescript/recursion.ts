474209e415be6f2c435cb4419cb80c0f89e4c637
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2020-04-11 01:13:26 +0300

    Recursion: recursively caamlize keys

---
+++ data_structures_algorithm_typescript/recursion.ts
@@ -0,0 +1,112 @@
+const factorial = (n: number) => {
+  if (n === 0) return 1;
+  return factorial(n - 1) * n;
+};
+
+//console.log(factorial(60));
+
+const camelize = (word: string) =>
+  word
+    .split("_")
+    .map((part, i) =>
+      i === 0 ? part.toLowerCase() : `${part[0].toUpperCase()}${part.slice(1)}`
+    )
+    .join("");
+
+const isObj = (obj: object) => obj !== null && obj.constructor === Object;
+
+const converKeysShallow = (obj: object) => {
+  const newObj = {};
+  Object.keys(obj).forEach((prop) => {
+    newObj[camelize(prop)] = obj[prop];
+  });
+  return newObj;
+};
+
+/* const convertKeys = (obj: object) => {
+  for (const k in obj) {
+    if (typeof obj[k] == "object" && obj[k] !== null) {
+      const newObj = {};
+      Object.keys(obj).forEach((prop) => {
+        newObj[camelize(prop)] = obj[prop];
+      });
+    } else {
+      return converKeysShallow(obj);
+    }
+    // do something...
+  }
+}; */
+
+const convertKeys = (obj: object) => {
+  if (isObj(obj)) {
+    return Object.keys(obj).reduce((acc, val) => {
+      return {
+        ...acc,
+        [camelize(val)]: convertKeys(obj[val]),
+      };
+    }, {});
+  }
+  return obj;
+};
+
+const someData = {
+  age_as_at_now: 45,
+  early_data: ["here", "and", "there"],
+  subject_taken: ["phy", "maths"],
+  course_mates: {
+    sonja_koivi: {
+      age_as_at_now: 99,
+      class_taken: 344,
+      info_from: {
+        at: "434",
+        created_at: "k",
+        updated_at: {
+          place_of_update: "Finland",
+          time_of_update: "232332",
+        },
+      },
+    },
+    sam_sake: {
+      age_as_at_now: 99,
+      class_taken: 542,
+    },
+    oyelowo_oyedayo: {
+      age_as_at_now: 99,
+      class_taken: 1044,
+    },
+    kaks_ppo: {},
+  },
+};
+
+const kk = convertKeys(someData);
+console.log(JSON.stringify(kk));
+
+/* const camelizeKeys = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => camelizeKeys(v));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce(
+      (result, key) => ({
+        ...result,
+        [camelize(key)]: camelizeKeys(obj[key]),
+      }),
+      {}
+    );
+  }
+  return obj;
+}; */
+
+const camelizeKeys = (obj: object) => {
+  if (isObj(obj)) {
+    return Object.keys(obj).reduce(
+      (result, key) => ({
+        ...result,
+        [camelize(key)]: camelizeKeys(obj[key]),
+      }),
+      {}
+    );
+  }
+  return obj;
+};
+
+//console.log(camelizeKeys(someData));
