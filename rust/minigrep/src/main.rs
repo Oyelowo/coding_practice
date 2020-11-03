f67e4c3b59dd568b0167cb1e39d5c26fa487694a
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2020-11-03 19:16:36 +0200

    Mini grep

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,3 +1,34 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = match   Config::new(&args) {
+        Ok(f)=>f,
+        Err(e)=> e,
+    };
+
+    println!("In file {}", config.filename);
+
+    let contents =
+        fs::read_to_string(config.filename).expect("Something wen wrong reading the file");
+
+    println!("With text: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+
+        let filename = args[2].clone();
+        Ok(Self { query, filename })
+    }
 }
