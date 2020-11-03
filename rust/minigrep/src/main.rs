94a32c5c51f5c7a86e40ac696921cb300cc7f94f
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2020-11-03 20:00:40 +0200

    Modularise into lib

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,34 +1,21 @@
-use std::{env, fs};
+
+use std::{env, process};
+
+use minigrep::{Config, run};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = match   Config::new(&args) {
-        Ok(f)=>f,
-        Err(e)=> e,
-    };
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("In file {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something wen wrong reading the file");
-
-    println!("With text: \n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-
-        let filename = args[2].clone();
-        Ok(Self { query, filename })
+        process::exit(1);
     }
+
 }
