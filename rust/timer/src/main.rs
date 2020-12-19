6c55e244f96e905a57c21b3e717f7c7baade4146
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2020-12-19 22:56:26 +0200

    Pinning to heap

--- rust/timer/src/main.rs
+++ rust/timer/src/main.rs
@@ -1,22 +1,7 @@
-use std::{marker::PhantomPinned, pin::Pin};
+use std::pin::Pin;
+use std::marker::PhantomPinned;
 
-fn main() {
-    // test1 is safe to move before we initialize it
-    let mut test1 = Test::new("test1");
-    // Notice how we shadow `test1` to prevent it from being accessed again
-    let mut test1 = unsafe {Pin::new_unchecked(&mut test1)};
-    Test::init(test1.as_mut());
-    
-    let mut test2 = Test::new("test2");
-    let mut test2 = unsafe {Pin::new_unchecked(&mut test2)};
-    Test::init(test2.as_mut());
-
-    println!("a: {}, b: {}", Test::a(test1.as_ref()), Test::b(test1.as_ref()));
-    std::mem::swap(test1.get_mut(), test2.get_mut());
-    test1.a = "I've totally changed now!".to_string();
-    println!("a: {}, b: {}", Test::a(test2.as_ref()), Test::b(test2.as_ref()));
-}
-
+// Pinning to the Heap
 #[derive(Debug)]
 struct Test {
     a: String,
@@ -25,18 +10,18 @@ struct Test {
 }
 
 impl Test {
-    fn new(txt: &str) -> Self {
-        Test {
+
+    fn new(txt: &str) -> Pin<Box<Self>> {
+        let t = Test {
             a: String::from(txt),
             b: std::ptr::null(),
-            _marker: PhantomPinned, // This makes our type `!Unpin`
-        }
-    }
+            _marker: PhantomPinned,
+        };
+        let mut boxed = Box::pin(t);
+        let self_ptr: *const String = &boxed.as_ref().a;
+        unsafe { boxed.as_mut().get_unchecked_mut().b = self_ptr };
 
-    fn init<'a>(self: Pin<&'a mut Self>) {
-        let self_ptr: *const String = &self.a;
-        let this = unsafe { self.get_unchecked_mut() };
-        this.b = self_ptr;
+        boxed
     }
 
     fn a<'a>(self: Pin<&'a Self>) -> &'a str {
@@ -47,3 +32,36 @@ impl Test {
         unsafe { &*(self.b) }
     }
 }
+
+pub fn main() {
+    let mut test1 = Test::new("test1");
+    let mut test2 = Test::new("test2");
+
+    println!("a: {}, b: {}",test1.as_ref().a(), test1.as_ref().b());
+    println!("a: {}, b: {}",test2.as_ref().a(), test2.as_ref().b());
+
+    
+    
+}
+
+
+
+
+
+/* use pin_utils::pin_mut; // `pin_utils` is a handy crate available on crates.io
+
+// A function which takes a `Future` that implements `Unpin`.
+fn execute_unpin_future(x: impl Future<Output = ()> + Unpin) { /* ... */ }
+
+let fut = async { /* ... */ };
+execute_unpin_future(fut); // Error: `fut` does not implement `Unpin` trait
+
+// Pinning with `Box`:
+let fut = async { /* ... */ };
+let fut = Box::pin(fut);
+execute_unpin_future(fut); // OK
+
+// Pinning with `pin_mut!`:
+let fut = async { /* ... */ };
+pin_mut!(fut);
+execute_unpin_future(fut); // OK */
