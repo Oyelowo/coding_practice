0434d1ac4854fa281f184d2fb7a4f261f94bb75e
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-01-02 22:07:13 +0200

    Tokio async: Manual copying

--- rust/tokio/examples/async-red-write.rs
+++ rust/tokio/examples/async-red-write.rs
@@ -1,8 +1,9 @@
-use tokio::fs::File;
 use tokio::{
     self,
     io::{self, AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
 };
+use tokio::{fs::File, net::TcpListener};
 
 fn main() {
     read().unwrap();
@@ -10,8 +11,7 @@ fn main() {
     write().unwrap();
     write_all().unwrap();
     read_copy().unwrap();
-
-
+    echo_server().unwrap();
 }
 
 #[tokio::main]
@@ -71,4 +71,67 @@ async fn read_copy() -> io::Result<()> {
     println!("Wrote the first {:?} bytes of 'some bytes'.", n);
 
     Ok(())
+}
+
+#[tokio::main]
+async fn echo_server() -> io::Result<()> {
+    let socket = TcpStream::connect("127.0.0.1:6142").await.unwrap();
+    let (mut rd, mut wr) = io::split(socket);
+
+    // Write data in the background
+    let write_task = tokio::spawn(async move {
+        wr.write_all(b"hello\r\n").await?;
+        wr.write_all(b"world\r\n").await?;
+
+        // Sometimes, the rust type inferencer needs
+        // a little help
+        Ok::<_, io::Error>(())
+    });
+
+    let mut buf = vec![0; 128];
+
+    loop {
+        let n = rd.read(&mut buf).await?;
+
+        if n == 0 {
+            break;
+        }
+        println!("GOT {:?}", &buf[..n]);
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn echo_server_tcp_split() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    // Return value of `Ok(0)` signifies that the remote has
+                    // closed
+                    Ok(0) => return,
+                    Ok(n) => {
+                        // Copy the data back to socket
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            // Unexpected socket error. There isn't much we can
+                            // do here so just stop processing.
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        // Unexpected socket error. There isn't much we can do
+                        // here so just stop processing.
+                        return;
+                    }
+                }
+            }
+        });
+    }
 }
