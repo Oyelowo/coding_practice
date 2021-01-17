048b6e4ef7ec59907e051601c13e3a153ee09bae
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-01-17 09:21:54 +0200

    Resolve members' teams

--- rust/modern_web/src/graphql_schema.rs
+++ rust/modern_web/src/graphql_schema.rs
@@ -63,11 +63,10 @@ pub struct Member {
     pub id: i32,
     pub name: String,
     pub knockouts: i32,
-
-    #[graphql(skip)]
-    pub team_id: i32,
+    pub team: Team,
 }
 
+#[derive(Debug)]
 pub struct Memberd {
     pub id: i32,
     pub name: String,
@@ -76,23 +75,12 @@ pub struct Memberd {
 
 #[Object]
 impl Memberd {
-        async fn id(&self) -> i32 {
-        self.id
-    }
-
-        async fn name(&self) -> String {
-        self.name.to_string()
-    }
-
-        async fn knockouts(&self) -> i32 {
-        self.knockouts
-    }
-
+ 
     async fn team(&self) -> Result<Option<Team>> {
         let pool = connect_db().await?;
         let team = sqlx::query_as!(
             Team,
-            "SELECT * FROM TEAMS WHERE id=$1", 1
+            "SELECT t.id, t.name FROM teams as t JOIN members as m ON m.team_id = t.id WHERE m.id=$1", self.id
         )
         .fetch_optional(&pool)
         .await?;
@@ -100,7 +88,6 @@ impl Memberd {
         Ok(team)
     }
 }
-
 
 #[derive(Default)]
 pub struct MembersQuery;
@@ -112,7 +99,8 @@ impl MembersQuery {
         // let members = sqlx::query_as!( "SELECT M.id, M.name, knockouts, team_id FROM MEMBERS AS M JOIN TEAMS AS T ON M.team_id = T.id")
         let mut members = sqlx::query_as!(
             Memberd,
-            "SELECT id, name, knockouts FROM MEMBERS AS M WHERE id=$1", id
+            "SELECT id, name, knockouts FROM MEMBERS AS M WHERE id=$1",
+            id
         )
         .fetch_optional(&pool)
         .await?;
@@ -120,41 +108,39 @@ impl MembersQuery {
         Ok(members)
     }
 
-    async fn members(&self) -> Result<Option<Vec<Member>>> {
+    /*   async fn members(&self) -> Result<Option<Vec<Member>>> {
         let pool = connect_db().await?;
         // let members = sqlx::query_as!( "SELECT M.id, M.name, knockouts, team_id FROM MEMBERS AS M JOIN TEAMS AS T ON M.team_id = T.id")
         let members = sqlx::query_as!(
-            Member,
             "SELECT M.id, M.name, knockouts, team_id FROM MEMBERS AS M"
         )
         .fetch_all(&pool)
         .await?;
         println!("vv{:?}", members);
         Ok(Some(members))
-    }
+    } */
 }
 
 #[derive(Debug, SimpleObject)]
 pub struct Team {
     pub id: i32,
     pub name: String,
-
-   // #[graphql(skip)]
-   //  pub members: Vec<Member>,
+    // #[graphql(skip)]
+    //  pub members: Vec<Member>,
 }
 #[derive(Debug, SimpleObject)]
 pub struct TeamWithMembers {
     pub id: i32,
     pub name: String,
 
-   // #[graphql(skip)]
-     pub members: Vec<Member>,
+    // #[graphql(skip)]
+    pub members: Vec<Member>,
 }
 
 #[derive(Default)]
 pub struct TeamQuery;
 
-#[Object]
+/* #[Object]
 impl TeamQuery {
     async fn team(&self, id: i32) -> Result<Option<TeamWithMembers>> {
         let pool = connect_db().await?;
@@ -164,7 +150,7 @@ impl TeamQuery {
         let teamsMembers = sqlx::query_as!(Member, "SELECT * FROM MEMBERS WHERE team_id = 1")
             .fetch_all(&pool) // -> Vec<{ country: String, count: i64 }>
             .await?;
-        let kk : TeamWithMembers = TeamWithMembers {
+        let kk: TeamWithMembers = TeamWithMembers {
             id: team.id,
             name: team.name,
             members: teamsMembers,
@@ -181,9 +167,9 @@ impl TeamQuery {
         Ok(Some(teams))
     }
 }
-
+ */
 #[derive(MergedObject, Default)]
-pub struct Query(MathQuery, MembersQuery, TeamQuery);
+pub struct Query(MathQuery, MembersQuery);
 
 #[warn(private_in_public)]
 pub type SchemaGraphQL = Schema<Query, EmptyMutation, EmptySubscription>;
