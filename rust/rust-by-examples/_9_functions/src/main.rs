fae202fe49cf739e81f5c32e8ce54795ce7a5ab4
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-06-18 17:33:13 +0300

    higher order function

--- rust/rust-by-examples/_9_functions/src/main.rs
+++ rust/rust-by-examples/_9_functions/src/main.rs
@@ -4,6 +4,11 @@ fn main() {
     closure_capturing();
     closure_move();
     closure_as_input_parameters();
+    type_anonymity();
+    input_functions();
+    closure_as_output_parameters();
+    iterator_any();
+    higher_order_function();
 }
 
 ////////////////////////////////////
@@ -224,6 +229,278 @@ fn closure_as_input_parameters() {
 
     println!("3 doubled: {}", apply_to_3(double));
 }
+
+/////////////////////////////////
+// Type anonymity
+/*
+Closures succinctly capture variables from enclosing scopes.
+Does this have any consequences? It surely does. Observe how using a closure as
+a function parameter requires generics, which is necessary because of how they are defined:
+
+*/
+
+// `F` must be generic.
+fn apply2<F>(f: F)
+where
+    F: FnOnce(),
+{
+    f();
+}
+/*
+When a closure is defined, the compiler implicitly creates a new anonymous
+structure to store the captured variables inside, meanwhile implementing the
+functionality via one of the traits: Fn, FnMut, or FnOnce for this unknown type.
+This type is assigned to the variable which is stored until calling.
+
+Since this new type is of unknown type, any usage in a function will require generics.
+However, an unbounded type parameter <T> would still be ambiguous and not be allowed.
+Thus, bounding by one of the traits: Fn, FnMut, or FnOnce (which it implements) is sufficient to specify its type.
+
+*/
+
+// `F` must implement `Fn` for a closure which takes no
+// inputs and returns nothing - exactly what is required
+// `F` must implement `Fn` for a closure which takes no
+// inputs and returns nothing - exactly what is required
+// for `print`.
+fn apply3<F>(f: F)
+where
+    F: Fn(),
+{
+    f();
+}
+
+fn type_anonymity() {
+    let x = 7;
+
+    // Capture `x` into an anonymous type and implement
+    // `Fn` for it. Store it in `print`.
+    let print = || println!("{}", x);
+
+    apply(print);
+}
+
+// Input functions
+/*
+Since closures may be used as arguments, you might wonder if the same can be said about functions.
+And indeed they can! If you declare a function that takes a closure as parameter,
+then any function that satisfies the trait bound of that closure can be passed as a parameter.
+*/
+// Define a function which takes a generic `F` argument
+// bounded by `Fn`, and calls it
+fn call_me<F: Fn()>(f: F) {
+    f();
+}
+
+// Define a wrapper function satisfying the `Fn` bound
+fn function() {
+    println!("I'm a function!");
+}
+
+fn input_functions() {
+    // Define a closure satisfying the `Fn` bound
+    let closure = || println!("I'm a closure!");
+
+    call_me(closure);
+    call_me(function);
+}
+/*
+As an additional note, the Fn, FnMut, and FnOnce traits dictate how a closure captures variables from the enclosing scope.
+*/
+
+/////////////////////////////////
+// As output parameters
+/*
+Closures as input parameters are possible, so returning closures as output parameters
+should also be possible. However, anonymous closure types are, by definition,
+unknown, so we have to use impl Trait to return them.
+
+The valid traits for returning a closure are:
+
+Fn
+FnMut
+FnOnce
+Beyond this, the move keyword must be used, which signals that all captures occur by value.
+This is required because any captures by reference would be dropped as soon as the function exited,
+leaving invalid references in the closure.
+*/
+fn create_fn() -> impl Fn() {
+    let text = "Fn".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnmut() -> impl FnMut() {
+    let text = "FnMut".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnonce() -> impl FnOnce() {
+    let text = "FnOnce".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn closure_as_output_parameters() {
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain();
+    fn_mut();
+    fn_once();
+}
+
+////////////////////////////////
+// Closures Examples std
+// Iterator::any
+/*
+Iterator::any is a function which when passed an iterator,
+will return true if any element satisfies the predicate. Otherwise false. Its signature:
+*/
+/* pub trait Iterator {
+    // The type being iterated over.
+    type Item;
+
+    // `any` takes `&mut self` meaning the caller may be borrowed
+    // and modified, but not consumed.
+    fn any<F>(&mut self, f: F) -> bool
+    where
+        // `FnMut` meaning any captured variable may at most be
+        // modified, not consumed. `Self::Item` states it takes
+        // arguments to the closure by value.
+        F: FnMut(Self::Item) -> bool,
+    {
+
+    }
+} */
+
+fn iterator_any() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vecs yields `&i32`. Destructure to `i32`.
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+
+    // `into_iter()` for vecs yields `i32`. No destructuring required.
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    // `iter()` for arrays yields `&i32`.
+    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    // `into_iter()` for arrays unusually yields `&i32`.
+    println!("2 in array2: {}", array2.into_iter().any(|&x| x == 2));
+}
+
+/////////////////////////////////////
+// Searching through iterators
+/*
+Iterator::find is a function which iterates over an iterator and
+searches for the first value which satisfies some condition.
+If none of the values satisfy the condition, it returns None. Its signature:
+*/
+
+/* pub trait Iterator {
+    // The type being iterated over.
+    type Item;
+
+    // `find` takes `&mut self` meaning the caller may be borrowed
+    // and modified, but not consumed.
+    fn find<P>(&mut self, predicate: P) -> Option<Self::Item> where
+        // `FnMut` meaning any captured variable may at most be
+        // modified, not consumed. `&Self::Item` states it takes
+        // arguments to the closure by reference.
+        P: FnMut(&Self::Item) -> bool {}
+} */
+
+fn iterator_find() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    // `iter()` for vecs yields `&i32`.
+    let mut iter = vec1.iter();
+    // `into_iter()` for vecs yields `i32`.
+    let mut into_iter = vec2.into_iter();
+
+    // `iter()` for vecs yields `&i32`, and we want to reference one of its
+    // items, so we have to destructure `&&i32` to `i32`
+    println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    // `into_iter()` for vecs yields `i32`, and we want to reference one of
+    // its items, so we have to destructure `&i32` to `i32`
+    println!("Find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    // `iter()` for arrays yields `&i32`
+    println!("Find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    // `into_iter()` for arrays unusually yields `&i32`
+    println!(
+        "Find 2 in array2: {:?}",
+        array2.into_iter().find(|&&x| x == 2)
+    );
+
+    let vec = vec![1, 9, 3, 3, 13, 2];
+
+    let index_of_first_even_number = vec.iter().position(|x| x % 2 == 0);
+    assert_eq!(index_of_first_even_number, Some(5));
+
+    let index_of_first_negative_number = vec.iter().position(|x| x < &0);
+    assert_eq!(index_of_first_negative_number, None);
+}
+
+////////////////////////////////////////////////////////////////
+// Higher Order Functions
+/*
+Rust provides Higher Order Functions (HOF). These are functions that take one or more
+functions and/or produce a more useful function. HOFs and lazy iterators give Rust its functional flavor.
+*/
+fn is_odd(n: u32) -> bool {
+    n % 2 == 1
+}
+
+fn higher_order_function() {
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+
+    // Imperative approach
+    // Declare accumulator variable
+    let mut acc = 0;
+    // Iterate: 0, 1, 2, ... to infinity
+    for n in 0.. {
+        // Square the number
+        let n_squared = n * n;
+
+        if n_squared >= upper {
+            // Break loop if exceeded the upper limit
+            break;
+        } else if is_odd(n_squared) {
+            // Accumulate value, if it's odd
+            acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
+
+    let sum_of_squares: u32 = (0..)
+        .map(|a| a * a)
+        .take_while(|&x| x <= upper)
+        .filter(|x| is_odd(*x))
+        .fold(0, |acc, val| acc + val);
+
+    println!("My solutionhahahah, {}", sum_of_squares);
+
+    // Functional approach
+    let sum_of_squared_odd_numbers: u32 = (0..)
+        .map(|n| n * n) // All natural numbers squared
+        .take_while(|&n_squared| n_squared < upper) // Below upper limit
+        .filter(|&n_squared| is_odd(n_squared)) // That are odd
+        .fold(0, |acc, n_squared| acc + n_squared); // Sum them
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+}
+
 ////////////////////////////////////////////////////////////////
 // Methods
 /*
