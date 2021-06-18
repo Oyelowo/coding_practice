04f02a14ee6c521dac218e614ca9284bc1e8e995
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-06-18 15:36:07 +0300

    Closure capturing and move

---
+++ rust/rust-by-examples/_9_functions/src/main.rs
@@ -0,0 +1,273 @@
+fn main() {
+    methods_1();
+    closures();
+    closure_capturing();
+    closure_move();
+}
+
+////////////////////////////////////
+// Closures
+/*
+Closures are functions that can capture the enclosing environment.
+For example, a closure that captures the x variable:
+
+
+|val| val + x
+The syntax and capabilities of closures make them very convenient
+for on the fly usage. Calling a closure is exactly like calling a function.
+However, both input and return types can be inferred and input variable names must be specified.
+
+Other characteristics of closures include:
+
+- using || instead of () around input variables.
+- optional body delimination ({}) for a single expression (mandatory otherwise).
+- the ability to capture the outer environment variables.
+
+*/
+
+fn closures() {
+    // Increment via closures and functions.
+    fn function(i: i32) -> i32 {
+        i + 1
+    }
+
+    // Closures are anonymous, here we are binding them to references
+    // Annotation is identical to function annotation but is optional
+    // as are the `{}` wrapping the body. These nameless functions
+    // are assigned to appropriately named variables.
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred = |i| i + 1;
+
+    let i = 1;
+    // Call the function and closures.
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+
+    // A closure taking no arguments which returns an `i32`.
+    // The return type is inferred.
+    let one = || 1;
+    println!("closure returning one: {}", one());
+}
+
+///////////////////////////////////////////////////////////////////
+// Capturing (Closure)
+/*
+Capturing
+Closures are inherently flexible and will do what the functionality requires
+to make the closure work without annotation. This allows capturing to flexibly
+adapt to the use case, sometimes moving and sometimes borrowing. Closures can capture variables:
+
+- by reference: &T
+- by mutable reference: &mut T
+- by value: T
+They preferentially capture variables by reference and only go lower when required.
+*/
+fn closure_capturing() {
+    use std::mem;
+
+    let color = String::from("green");
+
+    // A closure to print `color` which immediately borrows (`&`) `color` and
+    // stores the borrow and closure in the `print` variable. It will remain
+    // borrowed until `print` is used the last time.
+    //
+    // `println!` only requires arguments by immutable reference so it doesn't
+    // impose anything more restrictive.
+    let print = || println!("`color`: {}", color);
+
+    // Call the closure using the borrow.
+    print();
+
+    // `color` can be borrowed immutably again, because the closure only holds
+    // an immutable reference to `color`.
+    let _reborrow = &color;
+
+    // A move or reborrow is allowed after the final use of `print`
+    let _color_moved = color;
+
+    let mut count = 0;
+    // A closure to increment `count` could take either `&mut count` or `count`
+    // but `&mut count` is less restrictive so it takes that. Immediately
+    // borrows `count`.
+    //
+    // A `mut` is required on `inc` because a `&mut` is stored inside. Thus,
+    // calling the closure mutates the closure which requires a `mut`.
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+
+    // Call the closure using a mutable borrow.
+    inc();
+
+    // The closure still mutably borrows `count` because it is called later.
+    // An attempt to reborrow will lead to an error.
+
+    // let _reborrow = &count;
+    // ^ TODO: try uncommenting this line.
+    inc();
+
+    // The closure no longer needs to borrow `&mut count`. Therefore, it is
+    // possible to reborrow without an error
+    let _count_reborrowed = &mut count;
+
+    // A non-copy type.
+    let movable = Box::new(3);
+
+    // `mem::drop` requires `T` so this must take by value. A copy type
+    // would copy into the closure leaving the original untouched.
+    // A non-copy must move and so `movable` immediately moves into
+    // the closure.
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+        mem::drop(movable);
+    };
+
+    // `consume` consumes the variable so this can only be called once.
+    consume();
+    // consume();
+    // ^ TODO: Try uncommenting this line.
+}
+
+// Using move before vertical pipes forces closure to take ownership of captured variables:
+fn closure_move() {
+    // `Vec` has non-copy semantics.
+    let haystack = vec![1, 2, 3];
+
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    // println!("There're {} elements in vec", haystack.len());
+    // ^ Uncommenting above line will result in compile-time error
+    // because borrow checker doesn't allow re-using variable after it
+    // has been moved.
+
+    // Removing `move` from closure's signature will cause closure
+    // to borrow _haystack_ variable immutably, hence _haystack_ is still
+    // available and uncommenting above line will not cause an error.
+}
+
+////////////////////////////////////////////////////////////////
+// Methods
+/*
+Methods are functions attached to objects.
+These methods have access to the data of the object and its other
+methods via the self keyword. Methods are defined under an impl block.
+*/
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+// Implementation block, all `Point` methods go in here
+impl Point {
+    // This is a static method
+    // Static methods don't need to be called by an instance
+    // These methods are generally used as constructors
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+
+    // Another static method, taking two arguments:
+    fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y }
+    }
+}
+
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+impl Rectangle {
+    // This is an instance method
+    // `&self` is sugar for `self: &Self`, where `Self` is the type of the
+    // caller object. In this case `Self` = `Rectangle`
+    fn area(&self) -> f64 {
+        // `self` gives access to the struct fields via the dot operator
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        // `abs` is a `f64` method that returns the absolute value of the
+        // caller
+        ((x1 - x2) * (y1 - y2)).abs()
+    }
+
+    fn perimeter(&self) -> f64 {
+        let Point { x: x1, y: y1 } = self.p1;
+        let Point { x: x2, y: y2 } = self.p2;
+
+        2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+    }
+
+    // This method requires the caller object to be mutable
+    // `&mut self` desugars to `self: &mut Self`
+    fn translate(&mut self, x: f64, y: f64) {
+        self.p1.x += x;
+        self.p2.x += x;
+
+        self.p1.y += y;
+        self.p2.y += y;
+    }
+}
+
+// `Pair` owns resources: two heap allocated integers
+struct Pair(Box<i32>, Box<i32>);
+
+impl Pair {
+    // This method "consumes" the resources of the caller object
+    // `self` desugars to `self: Self`
+    fn destroy(self) {
+        // Destructure `self`
+        let Pair(first, second) = self;
+
+        println!("Destroying Pair({}, {})", first, second);
+
+        // `first` and `second` go out of scope and get freed
+    }
+}
+
+fn methods_1() {
+    let rectangle = Rectangle {
+        // Static methods are called using double colons
+        p1: Point::origin(),
+        p2: Point::new(3.0, 4.0),
+    };
+
+    // Instance methods are called using the dot operator
+    // Note that the first argument `&self` is implicitly passed, i.e.
+    // `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+    println!("Rectangle perimeter: {}", rectangle.perimeter());
+    println!("Rectangle area: {}", rectangle.area());
+
+    let mut square = Rectangle {
+        p1: Point::origin(),
+        p2: Point::new(1.0, 1.0),
+    };
+
+    // Error! `rectangle` is immutable, but this method requires a mutable
+    // object
+    // rectangle.translate(1.0, 0.0);
+    // TODO ^ Try uncommenting this line
+
+    // Okay! Mutable objects can call mutable methods
+    square.translate(1.0, 1.0);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+
+    pair.destroy();
+
+    // Error! Previous `destroy` call "consumed" `pair`
+    //pair.destroy();
+    // TODO ^ Try uncommenting this line
+}
+
+// Functions that "don't" return a value, actually return the unit type `()`
+
+/*
+// When a function returns `()`, the return type can be omitted from the
+// signature
+*/
