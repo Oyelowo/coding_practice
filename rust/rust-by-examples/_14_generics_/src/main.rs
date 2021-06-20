6ff68b974eb0bfc97dd0843678e4a2ec99489564
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-06-20 08:09:13 +0300

    bounds test case

--- rust/rust-by-examples/_14_generics_/src/main.rs
+++ rust/rust-by-examples/_14_generics_/src/main.rs
@@ -276,6 +276,32 @@ mod bounds {
         t.area()
     }
 
+    // Testcase: empty bounds
+    /*
+    A consequence of how bounds work is that even if a trait doesn't
+    include any functionality, you can still use it as a bound. Eq and
+    Copy are examples of such traits from the std library.
+        */
+    struct Cardinal;
+    struct BlueJay;
+    struct Turkey;
+
+    trait Red {}
+    trait Blue {}
+
+    impl Red for Cardinal {}
+    impl Blue for BlueJay {}
+
+    // These functions are only valid for types which implement these
+    // traits. The fact that the traits are empty is irrelevant.
+    fn red<T: Red>(_: &T) -> &'static str {
+        "red"
+    }
+
+    fn blue<T: Blue>(_: &T) -> &'static str {
+        "blue"
+    }
+
     pub fn run() {
         // Error! `Vec<T>` does not implement `Display`. This
         // specialization will fail.
@@ -297,6 +323,18 @@ mod bounds {
         //println!("Area: {}", area(&_triangle));
         // ^ TODO: Try uncommenting these.
         // | Error: Does not implement either `Debug` or `HasArea`.
+
+        let cardinal = Cardinal;
+        let blue_jay = BlueJay;
+        let _turkey = Turkey;
+
+        // `red()` won't work on a blue jay nor vice versa
+        // because of the bounds.
+        println!("A cardinal is {}", red(&cardinal));
+        println!("A blue jay is {}", blue(&blue_jay));
+        
+        //println!("A turkey is {}", red(&_turkey));
+        // ^ TODO: Try uncommenting this line.
     }
 
     /*
