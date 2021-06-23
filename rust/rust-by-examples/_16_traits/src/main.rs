93dd2960d51606e38d1f8f992f7add74d3ea72db
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-06-23 21:06:28 +0300

    impl trait

--- rust/rust-by-examples/_16_traits/src/main.rs
+++ rust/rust-by-examples/_16_traits/src/main.rs
@@ -4,6 +4,74 @@ fn main() {
     operator_overloading::main();
     drop::main();
     iterators::main();
+    impl_trait::main();
+}
+
+mod impl_trait {
+
+    /*
+        If your function returns a type that implements MyTrait,
+        you can write its return type as -> impl MyTrait. This can help simplify
+        your type signatures quite a lot!
+    */
+
+    use std::iter;
+    use std::vec::IntoIter;
+
+    // This function combines two `Vec<i32>` and returns an iterator over it.
+    // Look how complicated its return type is!
+    fn combine_vecs_explicit_return_type(
+        v: Vec<i32>,
+        u: Vec<i32>,
+    ) -> iter::Cycle<iter::Chain<IntoIter<i32>, IntoIter<i32>>> {
+        v.into_iter().chain(u.into_iter()).cycle()
+    }
+
+    // This is the exact same function, but its return type uses `impl Trait`.
+    // Look how much simpler it is!
+    fn combine_vecs(v: Vec<i32>, u: Vec<i32>) -> impl Iterator<Item = i32> {
+        v.into_iter().chain(u.into_iter()).cycle()
+    }
+
+    pub fn main() {
+        let v1 = vec![1, 2, 3];
+        let v2 = vec![4, 5];
+        let mut v3 = combine_vecs(v1, v2);
+        assert_eq!(Some(1), v3.next());
+        assert_eq!(Some(2), v3.next());
+        assert_eq!(Some(3), v3.next());
+        assert_eq!(Some(4), v3.next());
+        assert_eq!(Some(5), v3.next());
+        println!("all done");
+
+        let plus_one = make_adder_function(1);
+        assert_eq!(plus_one(2), 3);
+    }
+
+    /*
+    More importantly, some Rust types can't be written out. For example,
+    every closure has its own unnamed concrete type. Before impl Trait syntax,
+     you had to allocate on the heap in order to return a closure. But now you
+     can do it all statically, like this:
+    */
+
+    // Returns a function that adds `y` to its input
+    fn make_adder_function(y: i32) -> impl Fn(i32) -> i32 {
+        let closure = move |x: i32| x + y;
+        closure
+    }
+
+    /*
+    You can also use impl Trait to return an iterator that uses map or
+    filter closures! This makes using map and filter easier. Because closure
+    types don't have names, you can't write out an explicit return type if
+    your function returns iterators with closures. But with impl Trait you
+    can do this easily:
+    */
+
+    fn double_positives<'a>(numbers: &'a Vec<i32>) -> impl Iterator<Item = i32> + 'a {
+        numbers.iter().filter(|x| x > &&0).map(|x| x * 2)
+    }
 }
 
 mod iterators {
@@ -35,7 +103,7 @@ mod iterators {
         //     * Otherwise, the next value is wrapped in `Some` and returned.
         // We use Self::Item in the return type, so we can change
         // the type without having to update the function signatures.
-        fn next(&mut self) -> Option<u32> {
+        fn next(&mut self) -> Option<Self::Item> {
             let new_next = self.curr + self.next;
 
             self.curr = self.next;
