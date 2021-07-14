3362c0c4f6d411362243c6d4815d7700b15618f4
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-07-14 09:17:22 +0300

    Implement an executor

--- rust/programming_rust_book/asynchronous_programming/src/main.rs
+++ rust/programming_rust_book/asynchronous_programming/src/main.rs
@@ -1,7 +1,13 @@
 use std::{
+    future::Future,
+    pin::Pin,
     sync::{Arc, Mutex},
-    task::Waker,
+    task::{Context, Poll, Waker},
 };
+
+use crossbeam::sync::Parker;
+use futures_lite::pin;
+use waker_fn::waker_fn;
 
 fn main() {
     println!("Hello, world!");
@@ -9,9 +15,23 @@ fn main() {
 
 pub struct SpawnBlocking<T>(Arc<Mutex<Shared<T>>>);
 
+impl<T: Send> Future for SpawnBlocking<T> {
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut guard = self.0.lock().unwrap();
+        if let Some(value) = guard.value.take() {
+            return Poll::Ready(value);
+        }
+
+        guard.waker = Some(cx.waker().clone());
+        Poll::Pending
+    }
+}
+
 /*
 The Shared struct must serve as a rendezvous between the future and
-the thread run‐ ning the closure, so it is owned by an Arc and
+the thread running the closure, so it is owned by an Arc and
  protected with a Mutex. (A synchro‐ nous mutex is fine here.)
  Polling the future checks whether value is present and saves
  the waker in waker if not. The thread that runs the closure
@@ -50,4 +70,20 @@ where
     });
 
     SpawnBlocking(inner)
+}
+
+fn block_on<F: Future>(future: F) -> F::Output {
+    let parker = Parker::new();
+    let unparker = parker.unparker().clone();
+    let waker = waker_fn(move || unparker.unpark());
+    let mut context = Context::from_waker(&waker);
+
+    pin!(future);
+
+    loop {
+        match future.as_mut().poll(&mut context) {
+            Poll::Ready(value) => return value,
+            Poll::Pending => parker.park(),
+        }
+    }
 }
