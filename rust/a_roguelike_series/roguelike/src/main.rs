a9cf25347b6560dcc8c139074be2f3fc6bafef7a
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-07-25 13:29:44 +0300

    Roguelike: part1 @

--- rust/a_roguelike_series/roguelike/src/main.rs
+++ rust/a_roguelike_series/roguelike/src/main.rs
@@ -1,4 +1,8 @@
-use tcod::{colors::*, console::*};
+use tcod::{
+    colors::*,
+    console::*,
+    input::{Key, KeyCode::*},
+};
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -21,12 +25,47 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+    // handle keys and exit game if needed
+
     while !tcod.root.window_closed() {
+        
         tcod.root.set_default_foreground(WHITE);
         // Then we clear the console of anything that we drew on the previous frame.
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root
+        .put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
+        
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let key = tcod.root.wait_for_keypress(true);
+
+    match key {
+        // movement keys
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true,
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        _ => {}
+    }
+    false
 }
