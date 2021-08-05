3869e2908fcb4cdfeeee1a7e747611e87db3739e
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-08-05 23:17:38 +0300

    rogue game: Abstract object creation

--- rust/a_roguelike_series/roguelike/src/bin/part2.rs
+++ rust/a_roguelike_series/roguelike/src/bin/part2.rs
@@ -1,22 +1,119 @@
 use tcod::{
-    colors,
-    console::{Offscreen, Root},
-    BackgroundFlag, Color, Console,
+    colors::{self, WHITE},
+    console::{blit, Offscreen, Root},
+    BackgroundFlag, Color, Console, FontLayout, FontType,
 };
 
-const SCREEN_WIDTH: u16 = 640;
-const SCREEN_HEIGHT: u16 = 480;
+const SCREEN_WIDTH: i32 = 120;
+const SCREEN_HEIGHT: i32 = 70;
+const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
     con: Offscreen,
 }
 
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen.
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    /// move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, console: &mut dyn Console) {
+        console.set_default_foreground(self.color);
+        console.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+    use tcod::input::{Key, KeyCode::*};
+
+    let key = tcod.root.wait_for_keypress(true);
+
+    match key {
+        // movement keys
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
+        _ => {}
+    }
+    false
+}
+
 fn main() {
-    let con = Offscreen::new(SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32);
-    let tcod = Tcod { root, con };
-    tcod.con.set_default_foreground(colors::WHITE);
-    tcod.con.clear();
-    tcod.con
-        .put_char(player_x, player_y, '@', BackgroundFlag::None);
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod Way")
+        .init();
+
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut tcod = Tcod { root, con };
+
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
+
+    while !tcod.root.window_closed() {
+        // Then we clear the console of anything that we drew on the previous frame.
+        tcod.con.clear();
+
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+
+        // blit the contents of "con" to the root console and present it
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
+
+        tcod.root.flush();
+
+        // handle keys and exit game if needed
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
+        if exit {
+            break;
+        }
+    }
 }
