8a6f5706203fb3e0bc1557641db7d0dbf4c4e488
Oyelowo Oyedayo <oyelowooyedayo@gmail.com>
2021-08-25 16:41:26 +0300

    Ethereum: solidity, NFT, polygon, IPS, hardhat

---
+++ blockchain/fullstack-eth-dev/digi-mall/config.js
@@ -0,0 +1,9 @@
+// Local
+/* export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+ */
+// Testnet
+export const nftmarketaddress = "0xb7DcC5296e5C910Df12915B8E62992473272eB18"
+export const nftaddress = "0x3ceB00dF6F3c5030F6A0429B31Be9842ccb4D052"
+
+
