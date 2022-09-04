ca9b1fa9508a50109c059768b47c6569deca5424
oyelowo <oyelowooyedayo@gmail.com>
2022-09-04 18:25:20 +0300

    Spawning

--- rust/async-and-tokio/src/bin/store_values.rs
+++ rust/async-and-tokio/src/bin/store_values.rs
@@ -1,26 +1,5 @@
-use std::ops::SubAssign;
-
 use mini_redis::{Connection, Frame};
-use tokio::{
-    net::{TcpListener, TcpStream},
-    task,
-};
-
-#[tokio::main]
-async fn main() {
-    // Bind the listener to the address
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    loop {
-        // The second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().await.unwrap();
-
-        // A new tasj is spawned for eacg inbound socket. The socket is
-        // moved to the new task and processed there.
-        tokio::spawn(async move {
-            process(socket).await;
-        });
-    }
-}
+use tokio::net::TcpStream;
 
 async fn process(socket: TcpStream) {
     use mini_redis::Command::{self, Get, Set};
@@ -44,7 +23,7 @@ async fn process(socket: TcpStream) {
             Get(cmd) => {
                 if let Some(value) = db.get(cmd.key()) {
                     // `Frame::Bulk` expects data to be of type `Bytes`. This
-                    // type will be covered later in the tutorial. For now,
+                    // type woill be covered later in the tutorial. For now,
                     // `&Vec<u8>` is converted to `Bytes` using `into()`.
                     Frame::Bulk(value.clone().into())
                 } else {
@@ -55,20 +34,6 @@ async fn process(socket: TcpStream) {
         };
 
         // Write the response to the client
-        connection.write_frame(&response).await.unwrap();
-    }
-}
-
-async fn processv1(socket: TcpStream) {
-    // The `Connection` lets us read/write redis **frames** instead of
-    // byte streams. The `Connection` type is defined by mini-redis.
-    let mut connection = Connection::new(socket);
-
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {frame:?}");
-
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
     }
 }
