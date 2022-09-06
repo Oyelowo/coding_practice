e3eb5b08561363b3f84591d120559225dcda553d
oyelowo <oyelowooyedayo@gmail.com>
2022-09-06 21:34:12 +0300

    Using tokio spawn for async mutltithreading client request

--- rust/async-and-tokio/src/bin/redis_connect.rs
+++ rust/async-and-tokio/src/bin/redis_connect.rs
@@ -1,17 +1,28 @@
+use std::sync::Arc;
+
 use mini_redis::{client, Result};
+use tokio::task;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+    let client = client::connect("127.0.0.1:6379").await?;
+    let client1 = Arc::new(::tokio::sync::Mutex::new(client));
 
-    for i in 0..1000 {
-        println!("fetching: {i}");
-        let key = format!("name{i}");
-        client.set(&key, format!("Oyelowo{i}").into()).await?;
+    for i in 0..1000000 {
+        let client_copy = client1.clone();
+        task::spawn(async move {
+            let mut client_lock = client_copy.lock().await;
+            println!("fetching: {i}");
+            let key = format!("name{i}");
+            client_lock
+                .set(&key, format!("Oyelowo{i}").into())
+                .await
+                .unwrap();
 
-        let result = client.get(&key).await?;
+            let result = client_lock.get(&key).await.unwrap();
 
-        println!("Get value from the server: result={result:?}");
+            println!("Get value from the server: result={result:?}");
+        });
     }
 
     Ok(())
