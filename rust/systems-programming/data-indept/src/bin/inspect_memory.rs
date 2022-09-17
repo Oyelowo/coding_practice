0685f0f6da6ce61c3db7b963587255e35f6f5684
oyelowo <oyelowooyedayo@gmail.com>
2022-09-17 14:58:43 +0300

    inspect memory

---
+++ rust/systems-programming/data-indept/src/bin/inspect_memory.rs
@@ -0,0 +1,12 @@
+// // Inspect program memory
+
+// use kernel32;
+// // use winapi;
+// use winapi::{
+//     DWORD, HANDLE, LPSYSTEM_INFO, LPVOID, MEMORY_BASIC_INFORMATION as MEMINFO, PVOID, SIZE_T,
+//     SYSTEM_INFO,
+// };
+
+fn main() {
+    // let this_pid: DWORD;
+}
