ec50865558c4607d04d515894782c33c7ac684a2
oyelowo <oyelowooyedayo@gmail.com>
2023-09-23 14:17:52 -0600

    capitaliseFirst

--- 2023_algo_ds/solutions/recursions/solutions/capitalizeFirst.js
+++ 2023_algo_ds/solutions/recursions/solutions/capitalizeFirst.js
@@ -12,4 +12,29 @@ function capitalizeFirst (array) {
   res.push(string);
   return res;
 }
-// capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
+
+
+
+const toUpperCase =(word) =>  word[0].toUpperCase() + word.substr(1);
+
+const capitalizeLastWord = (words) =>  toUpperCase(words.slice(words.length - 1)[0]);
+
+function capitalizeFirst2(words){
+  if (words.length === 1) return [capitalizeLastWord(words)];
+  // console.log("words.slice(0, -1)", words.slice(0, -1));
+  const res = capitalizeFirst2(words.slice(0, -1));
+  
+  // console.log("AFTER...words.slice(0, -1)", words.slice(0, -1));
+  // console.log("RES", res);
+  const lastWordCapped = capitalizeLastWord(words);
+  // console.log("lastWordCapped", lastWordCapped);
+  res.push(lastWordCapped);
+  return res;
+}
+
+console.log(capitalizeFirst2(['car','taco','banana'])); // ['Car','Taco','Banana']
+// console.log("rere".slice(-2))
+// console.log("first"[0].substring(2))
+
+// console.log(['car','taco','banana'].slice(2)[0].substring(1))
