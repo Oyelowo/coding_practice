af7641b9729d435e4181f71237d782ecbbaa639c
oyelowo <oyelowooyedayo@gmail.com>
2024-03-21 22:13:36 -0600

    Contains duplicate

---
+++ 2024_dsa/practise/array/contains_duplicate.py
@@ -0,0 +1,29 @@
+import typing
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        lookup = {}
+        for n in nums:
+            lookup[n] = lookup.get(n, 0) + 1
+            print(lookup)
+            print(lookup.get(n, 0))
+            if lookup.get(n, 0) > 1:
+                return True
+        return False
+
+
+dup = Solution().containsDuplicate(
+    [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        6,
+    ]
+)
+
+print(dup)
