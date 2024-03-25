c3d17f8240c310b46177f09322681ca863803453
oyelowo <oyelowooyedayo@gmail.com>
2024-03-25 15:01:30 -0600

    merge while and foor loop

--- 2024_dsa/theory/merge.py
+++ 2024_dsa/theory/merge.py
@@ -31,6 +31,52 @@ def merge_sort_recursion(arr):
     return merge_arrays(left, right)
 
 
+def merge_sort_iterative_while_loop(arr):
+    n = len(arr)
+    steps = 1
+    while steps < n:
+        for i in range(0, n, 2 * steps):
+            left = arr[i : i + steps]
+            right = arr[i + steps : i + 2 * steps]
+            merged = merge_arrays(left, right)
+            for x, el in enumerate(merged):
+                arr[i + x] = el
+        steps *= 2
+    return arr
+
+
+print("ITERATIVE WHILE LOOP")
+arx = merge_sort_iterative_while_loop([3, 56, 78, 2, 1, 4, 6])
+print(arx)
+arx = merge_sort_iterative_while_loop([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+print(arx)
+arx = merge_sort_iterative_while_loop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(arx)
+
+
+def merge_sort_iterative_for_loop(arr):
+    n = len(arr)
+    steps = 1
+    for i in range(0, n, steps):
+        for j in range(0, n, 2 * steps):
+            left = arr[j : j + steps]
+            right = arr[j + steps : j + 2 * steps]
+            merged = merge_arrays(left, right)
+            for x, el in enumerate(merged):
+                arr[j + x] = el
+        steps *= 2
+    return arr
+
+
+print("ITERATIVE FOR LOOP")
+arx = merge_sort_iterative_for_loop([3, 56, 78, 2, 1, 4, 6])
+print(arx)
+arx = merge_sort_iterative_for_loop([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+print(arx)
+arx = merge_sort_iterative_for_loop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(arx)
+
+print("RECURSION")
 arx = merge_sort_recursion([3, 56, 78, 2, 1, 4, 6])
 print(arx)
 arx = merge_sort_recursion([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
