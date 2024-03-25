f3170c913676e909689a21ad3d94fd9256e0a7f0
oyelowo <oyelowooyedayo@gmail.com>
2024-03-25 13:01:58 -0600

    Bubble

---
+++ 2024_dsa/theory/bubble.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j + 1] < arr[j]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+arx = bubble_sort([3, 56, 78, 2, 1, 4, 6])
+print(arx)
+arx = bubble_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+print(arx)
+arx = bubble_sort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(arx)
