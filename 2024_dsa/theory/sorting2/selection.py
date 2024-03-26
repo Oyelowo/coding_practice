5f30a36292789d5bfd7b954e76f97e031e817826
oyelowo <oyelowooyedayo@gmail.com>
2024-03-26 17:46:07 -0600

    Selection sort

---
+++ 2024_dsa/theory/sorting2/selection.py
@@ -0,0 +1,20 @@
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        current_min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[current_min_index]:
+                current_min_index = j
+
+        arr[i], arr[current_min_index] = arr[current_min_index], arr[i]
+
+    return arr
+
+
+arx = selection_sort([3, 56, 78, 2, 1, 4, 6])
+print(arx)
+arx = selection_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+print(arx)
+arx = selection_sort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(arx)
